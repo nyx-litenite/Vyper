7da14dfb779e8b5561643ac21ec274f61ac811b8
nyx-litenite <lite.nite628@gmail.com>
2020-11-24 22:15:06 -0500

    cleanup, quit/reset button

--- vyper.v
+++ vyper.v
@@ -1,14 +1,14 @@
-import term.ui as tui
+import term.ui as termui
 import rand
 
 const (
 	block_size = 1
-	green      = tui.Color{0, 255, 0}
-	grey       = tui.Color{150, 150, 150}
-	white      = tui.Color{255, 255, 255}
-	blue       = tui.Color{0, 0, 255}
-	red        = tui.Color{255, 0, 0}
-	black      = tui.Color{0, 0, 0}
+	green      = termui.Color{0, 255, 0}
+	grey       = termui.Color{150, 150, 150}
+	white      = termui.Color{255, 255, 255}
+	blue       = termui.Color{0, 0, 255}
+	red        = termui.Color{255, 0, 0}
+	black      = termui.Color{0, 0, 0}
 )
 
 enum Orientation {
@@ -44,11 +44,15 @@ fn (v Vec) facing() Orientation {
 	return result
 }
 
-fn (mut v Vec) randomize(width int, height int) {
-	v.x = rand.int_in_range(block_size, width - block_size)
-	v.y = rand.int_in_range(block_size, height - block_size)
+fn (mut v Vec) randomize(min_x int, min_y int, max_x int, max_y int) {
+	v.x = rand.int_in_range(min_x, max_x)
+	v.y = rand.int_in_range(min_y, max_y)
 }
 
+// ~ fn (mut v Vec) randomize(width int, height int) {
+// ~ v.x = rand.int_in_range(block_size, width - block_size)
+// ~ v.y = rand.int_in_range(block_size, height - block_size)
+// ~ }
 fn (mut v Vec) r_velocity(max_x int, max_y int) {
 	v.x = rand.int_in_range(-1 * block_size, block_size)
 	v.y = rand.int_in_range(-1 * block_size, block_size)
@@ -60,7 +64,7 @@ mut:
 	x: block_size
 	y: block_size
 }
-	color  tui.Color = green
+	color  termui.Color = green
 	facing Orientation = .top
 }
 
@@ -105,33 +109,19 @@ fn (mut s Snake) impact(direction Orientation) {
 
 fn (mut s Snake) move() {
 	mut i := s.body.len - 1
+	width := s.app.width
+	height := s.app.height
 	for i = s.body.len - 1; i >= 0; i-- {
 		mut piece := s.body[i]
 		if i > 0 {
 			piece.pos = s.body[i - 1].pos
 			piece.facing = s.body[i - 1].facing
 		} else {
-			/*
-			piece.facing = direction
-			
-			match piece.facing {
-				.top {
-					piece.pos.y = if piece.pos.y > block_size { piece.pos.y - block_size } else { piece.pos.y }
-				}
-				.right {
-					piece.pos.x = if piece.pos.x < s.app.width - block_size { piece.pos.x + block_size } else { piece.pos.x }
-				}
-				.bottom {
-					piece.pos.y = if piece.pos.y < s.app.height - block_size { piece.pos.y + block_size } else { piece.pos.y }
-				}
-				.left {
-					piece.pos.x = if piece.pos.x > block_size { piece.pos.x - block_size } else { piece.pos.x }
-				}
-			}
-			*/
 			piece.facing = s.direction
-			piece.pos.x += s.velocity.x
-			piece.pos.y += s.velocity.y
+			new_x := piece.pos.x + s.velocity.x
+			new_y := piece.pos.y + s.velocity.y
+			piece.pos.x += if new_x > block_size && new_x < width - block_size { s.velocity.x } else { 0 }
+			piece.pos.y += if new_y > block_size && new_y < height - block_size { s.velocity.y } else { 0 }
 		}
 		s.body[i] = piece
 	}
@@ -178,8 +168,8 @@ fn (s Snake) get_tail() BodyPart {
 
 fn (mut s Snake) randomize() {
 	mut pos := s.get_head().pos
-	pos.randomize(s.app.width, s.app.height)
-	s.velocity.r_velocity(1, 1)
+	pos.randomize(block_size, block_size, s.app.width, s.app.height)
+	s.velocity.randomize(-1 * block_size, -1 * block_size, block_size, block_size) // r_velocity(1, 1)
 	s.direction = s.velocity.facing()
 	s.body[0].pos = pos
 }
@@ -203,17 +193,17 @@ mut:
 	y: block_size
 }
 	captured bool
-	color    tui.Color = grey
+	color    termui.Color = grey
 	app      &App
 }
 
 fn (mut r Rat) randomize() {
-	r.pos.randomize(r.app.width, r.app.height)
+	r.pos.randomize(2 * block_size, 2 * block_size, r.app.width - block_size, r.app.height - block_size)
 }
 
 struct App {
 mut:
-	tui    &tui.Context = 0
+	termui &termui.Context = 0
 	snake  Snake
 	rat    Rat
 	width  int
@@ -222,25 +212,31 @@ mut:
 	state  GameState = .game
 }
 
-fn init(x voidptr) {
-	mut app := &App(x)
-	w, h := app.tui.window_width, app.tui.window_height
-	app.width = w
-	app.height = h
+fn (mut a App) new_game() {
 	mut snake := Snake{
 		body: []BodyPart{len: 1, init: BodyPart{}}
-		app: app
+		app: a
 	}
 	snake.randomize()
 	mut rat := Rat{
-		app: app
+		app: a
 	}
 	rat.randomize()
-	app.snake = snake
-	app.rat = rat
+	a.snake = snake
+	a.rat = rat
+	a.state = .game
+	a.redraw = true
 }
 
-fn event(e &tui.Event, x voidptr) {
+fn init(x voidptr) {
+	mut app := &App(x)
+	w, h := app.termui.window_width, app.termui.window_height
+	app.width = w
+	app.height = h
+	app.new_game()
+}
+
+fn event(e &termui.Event, x voidptr) {
 	mut app := &App(x)
 	match e.typ {
 		.mouse_down {}
@@ -252,13 +248,14 @@ fn event(e &tui.Event, x voidptr) {
 				.down, .s { app.move_snake(.bottom) }
 				.left, .a { app.move_snake(.left) }
 				.right, .d { app.move_snake(.right) }
+				.r { app.new_game() }
 				.c {}
-				.escape { exit(0) }
+				.p { app.state = if app.state == .game { GameState.pause } else { GameState.game } }
+				.escape, .q { exit(0) }
 				else { exit(0) }
 			}
 			if e.code == .c {
-			}
-			else if e.code == .escape {
+			} else if e.code == .escape {
 				exit(0)
 			}
 		}
@@ -268,37 +265,55 @@ fn event(e &tui.Event, x voidptr) {
 }
 
 fn (mut a App) update() {
-	a.snake.move()
-	overlap := a.snake.check_overlap()
-	if overlap {
-		a.state = .gameover
+	if a.state == .game {
+		a.snake.move()
+		if a.snake.check_overlap() {
+			a.state = .gameover
+			return
+		}
+		if a.check_capture() {
+			a.rat.randomize()
+			a.snake.grow()
+		}
 	}
+}
+
+fn (mut a App) draw() {
+	// reset screen
+	a.termui.clear()
+	a.termui.set_bg_color(white)
+	a.termui.draw_empty_rect(1, 1, a.width, a.height)
+	a.termui.set_bg_color(black)
+	a.termui.draw_rect(2, 2, a.width - 1, a.height - 1)
+	// determine what to draw
+	match a.state {
+		.gameover {
+			a.draw_gameover()
+			a.redraw = false
+		}
+		.pause {
+			a.draw_pause()
+		}
+		else {
+			a.redraw = true
+		}
+	}
+	if a.redraw {
+		$if verbose ? {
+			a.draw_debug()
+		}
+		a.draw_score()
+		a.draw_rat()
+		a.draw_snake()
+	}
+	a.termui.reset_bg_color()
+	a.termui.flush()
 }
 
 fn frame(x voidptr) {
 	mut app := &App(x)
-	if !app.redraw {
-		return
-	}
-	app.tui.clear()
 	app.update()
-	if app.state == .gameover {
-		app.draw_gameover()
-		app.redraw = false
-	} else {
-		app.draw_score()
-		app.draw_rat()
-		app.draw_snake()
-		$if verbose ? {
-			app.draw_debug()
-		}
-		if app.check_capture() {
-			app.rat.randomize()
-			app.snake.grow()
-		}
-	}
-	app.tui.reset_bg_color()
-	app.tui.flush()
+	app.draw()
 }
 
 fn (mut a App) move_snake(direction Orientation) {
@@ -307,22 +322,24 @@ fn (mut a App) move_snake(direction Orientation) {
 
 fn (mut a App) draw_snake() {
 	for part in a.snake.get_body() {
-		a.tui.set_bg_color(part.color)
-		text := match part.facing {
-			.top { '^' }
-			.bottom { 'v' }
-			.right { '>' }
-			.left { '<' }
+		a.termui.set_bg_color(part.color)
+		a.termui.draw_rect(part.pos.x, part.pos.y, part.pos.x + block_size, part.pos.y + block_size)
+		$if verbose ? {
+			text := match part.facing {
+				.top { '^' }
+				.bottom { 'v' }
+				.right { '>' }
+				.left { '<' }
+			}
+			a.termui.set_color(white)
+			a.termui.draw_text(part.pos.x, part.pos.y, text)
 		}
-		a.tui.draw_rect(part.pos.x, part.pos.y, part.pos.x + block_size, part.pos.y + block_size)
-		a.tui.set_color(white)
-		a.tui.draw_text(part.pos.x, part.pos.y, text)
 	}
 }
 
 fn (mut a App) draw_rat() {
-	a.tui.set_bg_color(a.rat.color)
-	a.tui.draw_rect(a.rat.pos.x, a.rat.pos.y, a.rat.pos.x + block_size, a.rat.pos.y + block_size)
+	a.termui.set_bg_color(a.rat.color)
+	a.termui.draw_rect(a.rat.pos.x, a.rat.pos.y, a.rat.pos.x + block_size, a.rat.pos.y + block_size)
 }
 
 fn (a App) check_capture() bool {
@@ -334,45 +351,50 @@ fn (a App) check_capture() bool {
 }
 
 fn (mut a App) draw_score() {
-	a.tui.set_bg_color(grey)
-	a.tui.set_color(blue)
+	a.termui.set_bg_color(grey)
+	a.termui.set_color(blue)
 	score := a.snake.length() - 1
-	a.tui.draw_text(a.width - (2 * block_size), block_size, '${score:03d}')
+	a.termui.draw_text(a.width - (2 * block_size), block_size, '${score:03d}')
+}
+
+fn (mut a App) draw_pause() {
+	a.termui.set_color(blue)
+	a.termui.draw_text(a.width / 2 - block_size, 3 * block_size, 'Paused!')
 }
 
 fn (mut a App) draw_debug() {
-	a.tui.set_color(blue)
-	a.tui.set_bg_color(grey)
+	a.termui.set_color(blue)
+	a.termui.set_bg_color(white)
 	snake := a.snake
-	rat := a.rat
-	a.tui.draw_text(block_size, block_size, 'Vx: ${snake.velocity.x:+02d} Vy: ${snake.velocity.y:+02d}')
-	a.tui.draw_text(block_size, 2 * block_size, 'F: $snake.direction')
+	a.termui.draw_text(block_size, 1 * block_size, 'Display_width: ${a.width:04d} Display_height: ${a.height:04d}')
+	a.termui.draw_text(block_size, 2 * block_size, 'Vx: ${snake.velocity.x:+02d} Vy: ${snake.velocity.y:+02d}')
+	a.termui.draw_text(block_size, 3 * block_size, 'F: $snake.direction')
 	snake_head := snake.get_head()
-	a.tui.draw_text(block_size, 4 * block_size, 'Sx: ${snake_head.pos.x:+03d} Sy: ${snake_head.pos.y:+03d}')
-	a.tui.draw_text(block_size, 5 * block_size, 'Rx: ${rat.pos.x:+03d} Ry: ${rat.pos.y:+03d}')
+	rat := a.rat
+	a.termui.draw_text(block_size, 4 * block_size, 'Sx: ${snake_head.pos.x:+03d} Sy: ${snake_head.pos.y:+03d}')
+	a.termui.draw_text(block_size, 5 * block_size, 'Rx: ${rat.pos.x:+03d} Ry: ${rat.pos.y:+03d}')
 }
 
 fn (mut a App) draw_gameover() {
-	a.tui.set_bg_color(white)
-	a.tui.draw_rect(0, 0, a.width, a.height)
-	a.tui.set_color(red)
-	a.tui.set_bg_color(black)
-	a.snake.body.clear()
+	a.termui.set_bg_color(white)
+	a.termui.draw_rect(0, 0, a.width, a.height)
+	a.termui.set_color(red)
+	a.termui.set_bg_color(black)
 	a.rat.pos = Vec{
 		x: -1
 		y: -1
 	}
-	a.tui.draw_text(block_size, block_size, '   #####                         #######                       ')
-	a.tui.draw_text(block_size, 2 * block_size, '  #     #   ##   #    # ######   #     # #    # ###### #####   ')
-	a.tui.draw_text(block_size, 3 * block_size, '  #        #  #  ##  ## #        #     # #    # #      #    #  ')
-	a.tui.draw_text(block_size, 4 * block_size, '  #  #### #    # # ## # #####    #     # #    # #####  #    #  ')
-	a.tui.draw_text(block_size, 5 * block_size, '  #     # ###### #    # #        #     # #    # #      #####   ')
-	a.tui.draw_text(block_size, 6 * block_size, '  #     # #    # #    # #        #     #  #  #  #      #   #   ')
-	a.tui.draw_text(block_size, 6 * block_size, '   #####  #    # #    # ######   #######   ##   ###### #    #  ')
+	a.termui.draw_text(block_size, 1 * block_size, '   #####                         #######                       ')
+	a.termui.draw_text(block_size, 2 * block_size, '  #     #   ##   #    # ######   #     # #    # ###### #####   ')
+	a.termui.draw_text(block_size, 3 * block_size, '  #        #  #  ##  ## #        #     # #    # #      #    #  ')
+	a.termui.draw_text(block_size, 4 * block_size, '  #  #### #    # # ## # #####    #     # #    # #####  #    #  ')
+	a.termui.draw_text(block_size, 5 * block_size, '  #     # ###### #    # #        #     # #    # #      #####   ')
+	a.termui.draw_text(block_size, 6 * block_size, '  #     # #    # #    # #        #     #  #  #  #      #   #   ')
+	a.termui.draw_text(block_size, 7 * block_size, '   #####  #    # #    # ######   #######   ##   ###### #    #  ')
 }
 
 mut app := &App{}
-app.tui = tui.init({
+app.termui = termui.init({
 	user_data: app
 	event_fn: event
 	frame_fn: frame
@@ -380,4 +402,4 @@ app.tui = tui.init({
 	hide_cursor: true
 	frame_rate: 15
 })
-app.tui.run()
+app.termui.run()
